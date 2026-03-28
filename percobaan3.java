4c24f96ef850c2882bd53d9be4364af157d646a0
TheLast3070 <bagindasiregarr74@gmail.com>
2026-03-28 18:24:56 +0700

    ketiga

--- percobaan3.java
+++ percobaan3.java
@@ -1,7 +1,7 @@
 public class percobaan3 {
         mahasiswa mahasiswa1 = new mahasiswa("Budi Asiap Mamang", "Teknik Informatika", "124140299",31, 90);
         mahasiswa mahasiswa2 = new mahasiswa("Jono Jamsut", "Teknik Informatika", "124140300", 22, 65);
-        
+        mahasiswa mahasiswa3 = new mahasiswa("yu zhong penguasa exp line", "Teknik Elektro", "124130999", 24, 75);
         void ceknilai(mahasiswa mhs){
             if (mhs.nilai >= 75) {
                 System.out.println(mhs.nama + " Lulus");
@@ -13,5 +13,6 @@ public class percobaan3 {
         percobaan3 percobaan = new percobaan3();
         percobaan.ceknilai(percobaan.mahasiswa1);
         percobaan.ceknilai(percobaan.mahasiswa2);
+        percobaan.ceknilai(percobaan.mahasiswa3);
     }
 }
---
+++ percobaan4.java
@@ -0,0 +1,9 @@
+public class percobaan4 {
+    public static void main(String[] args) {
+        String[] mahasiswa = {"Budi Asiap Mamang", "Jono Jamsut", "yu zhong penguasa exp line", "Doni Mokel", "Asep salto"};
+            for (int i = 0; i < mahasiswa.length; i++) {
+                System.out.println("Mahasiswa ke-" + (i+1) + " : " +
+                mahasiswa[i]);
+        }
+    }
+}
